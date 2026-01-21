f62439297789d372b80b6ce88494b477cdfbbfbd
sanglap acharyya <sanglapacharyya@sanglaps-MacBook-Pro.local>
2026-01-21 09:35:10 +0530

    commiting all file of structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sanglap Acharyya",
+    author_email="sanglapacharyya.aiml@gmail.com",
+    packages=find_packages()
+)
